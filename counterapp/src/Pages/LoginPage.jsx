117341805d643f0a3863c9f57497443b436c171b
morevishal99 <morevishal060298@gmail.com>
2023-07-26 11:15:31 +0530

    login and signup page completed

--- counterapp/src/Pages/LoginPage.jsx
+++ counterapp/src/Pages/LoginPage.jsx
@@ -8,41 +8,51 @@ const LoginPage = () => {
   const [password, setpassword] = useState('');
   const [currentPassword, setcurrentPassword] = useState("");
   const [warnigntext, setwarnigntext] = useState(false);
+  const usersDetails = JSON.parse(localStorage.getItem("userDetail")) || [] // get all user data  form localStorage
+  // console.log('usersDetails: ', usersDetails);
+
 
   const handlFormData = () => {
-    // alert("hello")
-
-  }
-  const handleRoute = () => {
-    navigate("/")
-  }
-  useEffect(() => {
-    if (password && currentPassword) {
-
-      if (password === currentPassword) {
-        setwarnigntext("It Seems Good")
-      } else {
-        setwarnigntext(" Password mismatch")
+    if (email && currentPassword && password) {   //if all fileds are not empty add user
+      let filteruser = usersDetails.filter((item) => item.email === email) //check if user is already present or not
+      console.log('filteruser: ', filteruser);
+      if (filteruser.length === 1) {  //if user present do nothing 
+        alert("Login Success")
+        navigate("/counter")
+        return
       }
     }
-  }, [password, currentPassword]);
-  return (
-    <>
-      <div className='container'>
-        <div>Login</div>
-        <form className='form'>
-          <input type="email" placeholder='Enter email' onChange={(e) => setemail(e.target.value)} />
-          <input type="password" placeholder='Enter password' onChange={(e) => setpassword(e.target.value)} />
-          <input type="text" placeholder='Re-Enter password' onChange={(e) => setcurrentPassword(e.target.value)} />
-          {warnigntext ? <h6 className='warnigntext'>{warnigntext}</h6> : ""}
-          <button disabled={password !== currentPassword} onClick={handlFormData}>Login</button>
-        </form>
-        <hr />
-        <h4>OR</h4>
-        <h3>New User? <span className='linkSpan' onClick={handleRoute}>SignUp</span> </h3>
-      </div>
-    </>
-  )
-}
+  }
+    const handleRoute = () => {
+      navigate("/")
+    }
+    useEffect(() => {
+      if (password && currentPassword) {
 
-export default LoginPage
+        if (password === currentPassword) {
+          setwarnigntext("It Seems Good")
+        } else {
+          setwarnigntext(" Password mismatch")
+        }
+      }
+    }, [password, currentPassword]);
+    return (
+      <>
+        <div className='container'>
+          <div>Login</div>
+          <form className='form'>
+            <input type="email" placeholder='Enter email' onChange={(e) => setemail(e.target.value)} />
+            <input type="password" placeholder='Enter password' onChange={(e) => setpassword(e.target.value)} />
+            <input type="text" placeholder='Re-Enter password' onChange={(e) => setcurrentPassword(e.target.value)} />
+            {warnigntext ? <h6 className='warnigntext'>{warnigntext}</h6> : ""}
+            <button disabled={password !== currentPassword} onClick={handlFormData}>Login</button>
+          </form>
+          <hr />
+          <h4>OR</h4>
+          <h3>New User? <span className='linkSpan' onClick={handleRoute}>SignUp</span> </h3>
+        </div>
+      </>
+    )
+  }
+
+  export default LoginPage
--- counterapp/src/Pages/SignupPage.jsx
+++ counterapp/src/Pages/SignupPage.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import "../Style/SignupPage.css"
 import { Link } from 'react-router';
 import { useNavigate } from "react-router-dom";
@@ -8,9 +8,31 @@ const SignupPage = () => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
   const [name, setname] = useState("");
-  
+  const usersDetails = JSON.parse(localStorage.getItem("userDetail")) || [] // get all user data  form localStorage
+  // console.log('usersDetails: ', usersDetails);
+
   const handlFormData = () => {
-    alert("hello")
+
+    if (email && name && password) {   //if all fileds are not empty add user
+      let filteruser = usersDetails.filter((item) => item.email === email) //check if user is already present or not
+      console.log('filteruser: ', filteruser);
+      if (filteruser.length === 1) {  //if user present do nothing 
+        alert("User already register please login")
+        navigate("/login")
+        return
+      }
+      else {   // is new user login add user data to locaStorage
+        let singleUserDetail = { //user object
+          email: email, name: name, password: password
+        }
+        usersDetails.push(singleUserDetail)
+        localStorage.setItem("userDetail", JSON.stringify(usersDetails))
+        alert("signUp successfull")
+        navigate("/login")
+      }
+    } else {
+      alert("Feilds Cannot be empty") //if fields are empty alert user 
+    }
   }
   const handleRoute = () => {
     navigate("/login")
@@ -21,9 +43,9 @@ const SignupPage = () => {
         <div>Signup</div>
         <form className='form'>
           {/* <h6>Email</h6> */}
-          <input type="text" placeholder='Enter name' onChange={(e)=>setname(e.target.value)} />
-          <input type="email" placeholder='Enter email' onChange={(e)=>setemail(e.target.value)} />
-          <input type="password" placeholder='Enter password' onChange={(e)=>setpassword(e.target.value)} />
+          <input type="text" placeholder='Enter name' value={name} onChange={(e) => setname(e.target.value)} />
+          <input type="email" placeholder='Enter email' value={email} onChange={(e) => setemail(e.target.value)} />
+          <input type="password" placeholder='Enter password' value={password} onChange={(e) => setpassword(e.target.value)} />
           <button onClick={handlFormData}>Submit</button>
         </form>
         <hr />
