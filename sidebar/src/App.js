97d8d76528bbd8a1cc633f5c38b9e1181e4344ec
morevishal99 <morevishal060298@gmail.com>
2023-07-27 13:22:36 +0530

    sidebar and sidebarComponents added

--- sidebar/src/App.js
+++ sidebar/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './Page/Sidebar';
-import Homepage from './Page/Homepage';
+// import Homepage from './Page/Homepage';
 
 function App() {
   return (
     <div className="App">
       {/* sidebar */}
-      <Homepage />
-      {/* <Sidebar /> */}
+      {/* <Homepage /> */}
+      <Sidebar />
     </div>
   );
 }
--- sidebar/src/Page/Sidebar.jsx
+++ sidebar/src/Page/Sidebar.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { AiOutlineUser, AiTwotoneBell } from "react-icons/ai";
+import { RxDashboard } from "react-icons/rx";
+import Dashboard from '../sidebarComponent/Dashboard'
+import Notification from '../sidebarComponent/Notification';
+import Profile from '../sidebarComponent/Profile'
+import "../Style/Homepage.css"
 const Sidebar = () => {
+    const [activeComponent, setActiveComponent] = useState("dashboard");
+
+    const handleComponentClick = (componentName) => {
+        setActiveComponent(componentName);
+    };
     return (
-        <div>
-            Sidebar
-        </div>
+        <>
+            <div className="sidebar">
+
+                <div className='sidebarMenu'>
+                    <ul className='sidebarMenu_ul' onClick={() => handleComponentClick('dashboard')}>
+                        <li ><RxDashboard /></li>
+                        <li>Dashboard</li>
+                    </ul>
+                    <ul className='sidebarMenu_ul' onClick={() => handleComponentClick('profile')} >
+                        <li ><AiOutlineUser /></li>
+                        <li>Profile</li>
+                    </ul>
+                    <ul className='sidebarMenu_ul' onClick={() => handleComponentClick('notification')} >
+                        <li ><AiTwotoneBell /></li>
+                        <li>Notification</li>
+                    </ul>
+                </div>
+
+                <div className="sidebarContent">
+                    {activeComponent === 'dashboard' && <Dashboard />}
+                    {activeComponent === 'profile' && <Profile />}
+                    {activeComponent === 'notification' && <Notification />}
+                </div>
+
+            </div>
+        </>
     )
 }
 
